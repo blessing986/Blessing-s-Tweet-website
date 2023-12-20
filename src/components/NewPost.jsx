2e3b20a2e35ef3bcadccebf33ecccb126cfe627b
Blessing U <blessing986@users.noreply.github.com>
2023-12-20 11:27:49 +0000

    chore: adding event listeners and working with states

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,12 +1,20 @@
+import { useState } from "react";
 import styles from "./NewPost.module.css";
 
 function NewPost() {
+  const [enteredBody, setEnteredBody] = useState("");
+
+  const changeBodyHandler = (event) => {
+    setEnteredBody(event.target.value);
+  };
+
   return (
     <form className={styles.form}>
       <p>
         <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} />
+        <textarea id="body" required rows={3} onChange={changeBodyHandler} />
       </p>
+      <p>{enteredBody}</p>
       <p>
         <label htmlFor="name">Your name</label>
         <input type="text" id="name" required />
