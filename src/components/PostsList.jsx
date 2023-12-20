e7e9763caafc3e6bdcf5b4a950ccd5693f1fc9fc
Blessing U <blessing986@users.noreply.github.com>
2023-12-20 12:25:16 +0000

    chore: implemented modal

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -2,10 +2,11 @@ import { useState } from "react";
 import NewPost from "./NewPost";
 import Post from "./Post";
 import styles from "./PostsList.module.css";
+import Modal from "./Modal";
 
-export default function PostsList() {
+export default function PostsList({ isTweeting, onStopTweeting }) {
   const [enteredBody, setEnteredBody] = useState("");
-  const [enteredAuthor, setEnteredAuthor] = useState();
+  const [enteredAuthor, setEnteredAuthor] = useState("");
 
   const bodyChangeHandler = (event) => {
     setEnteredBody(event.target.value);
@@ -17,10 +18,15 @@ export default function PostsList() {
 
   return (
     <>
-      <NewPost
-        onBodyChange={bodyChangeHandler}
-        onAuthorChange={authorChangeHandler}
-      />
+      {isTweeting && (
+        <Modal onClose={onStopTweeting}>
+          <NewPost
+            onBodyChange={bodyChangeHandler}
+            onAuthorChange={authorChangeHandler}
+          />
+        </Modal>
+      )}
+
       <ul className={styles.posts}>
         <Post author={enteredAuthor} body={enteredBody} />
         <Post author="Teddy" body="I love you" />
