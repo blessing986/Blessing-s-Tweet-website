850d84c98ac46a15cc308ac972867ee8c6f5081f
Blessing U <blessing986@users.noreply.github.com>
2023-12-26 14:31:52 +0000

    chore: handling form submmission

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,35 +1,18 @@
-import { useState } from "react";
 import NewPost from "./NewPost";
 import Post from "./Post";
 import styles from "./PostsList.module.css";
 import Modal from "./Modal";
 
 export default function PostsList({ isTweeting, onStopTweeting }) {
-  const [enteredBody, setEnteredBody] = useState("");
-  const [enteredAuthor, setEnteredAuthor] = useState("");
-
-  const bodyChangeHandler = (event) => {
-    setEnteredBody(event.target.value);
-  };
-
-  const authorChangeHandler = (event) => {
-    setEnteredAuthor(event.target.value);
-  };
-
   return (
     <>
       {isTweeting && (
         <Modal onClose={onStopTweeting}>
-          <NewPost
-            onBodyChange={bodyChangeHandler}
-            onAuthorChange={authorChangeHandler}
-            onCancel={onStopTweeting}
-          />
+          <NewPost onCancel={onStopTweeting} />
         </Modal>
       )}
 
       <ul className={styles.posts}>
-        <Post author={enteredAuthor} body={enteredBody} />
         <Post author="Teddy" body="I love you" />
       </ul>
     </>
