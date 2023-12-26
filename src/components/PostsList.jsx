ef25e2fd8d2f36fe0580aaa2c390892b732f1e9e
Blessing U <blessing986@users.noreply.github.com>
2023-12-26 17:37:45 +0000

    chore: handling loading state

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -6,12 +6,14 @@ import { useEffect, useState } from "react";
 
 export default function PostsList({ isTweeting, onStopTweeting }) {
   const [posts, setPosts] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
 
   useEffect(() => {
     async function fetchPosts() {
       const response = await fetch("http://localhost:8080/posts");
       const resData = await response.json();
       setPosts(resData.posts);
+      setIsFetching(false);
     }
     fetchPosts();
   }, []);
@@ -35,17 +37,22 @@ export default function PostsList({ isTweeting, onStopTweeting }) {
         </Modal>
       )}
 
-      {posts.length > 0 && (
+      {!isFetching && posts.length > 0 && (
         <ul className={styles.posts}>
           {posts.map((post) => (
             <Post key={post.body} author={post.author} body={post.body} />
           ))}
         </ul>
       )}
-      {posts.length === 0 && (
+      {!isFetching && posts.length === 0 && (
         <div style={{ textAlign: "center", color: "white" }}>
           <h2>There are no posts yet.</h2>
           <p>Start adding some!</p>
+        </div>
+      )}
+      {isFetching && (
+        <div style={{ textAlign: "center", color: "white" }}>
+          <p>Loading posts...</p>{" "}
         </div>
       )}
     </>
