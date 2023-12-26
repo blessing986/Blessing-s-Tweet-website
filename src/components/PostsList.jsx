86067ed3a8a3f56d6f4ab15a6d47482a69d2b97f
Blessing U <blessing986@users.noreply.github.com>
2023-12-26 14:43:39 +0000

    chore: updating state based on previous state

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -2,13 +2,20 @@ import NewPost from "./NewPost";
 import Post from "./Post";
 import styles from "./PostsList.module.css";
 import Modal from "./Modal";
+import { useState } from "react";
 
 export default function PostsList({ isTweeting, onStopTweeting }) {
+  const [posts, setPosts] = useState([]);
+
+  function addPostHandler(postData) {
+    setPosts((existingPosts) => [postData, ...existingPosts]);
+  }
+
   return (
     <>
       {isTweeting && (
         <Modal onClose={onStopTweeting}>
-          <NewPost onCancel={onStopTweeting} />
+          <NewPost onCancel={onStopTweeting} onAddPost={addPostHandler} />
         </Modal>
       )}
 
